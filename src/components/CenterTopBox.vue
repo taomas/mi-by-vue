6293977b480df6f53bda04f68272c6a9bc5861d0
liutao <947809647@qq.com>
2016-08-18 18:04:45 +0800

    update centerTopBox

--- src/components/CenterTopBox.vue
+++ src/components/CenterTopBox.vue
@@ -1,33 +1,35 @@
 <template>
-  <div class="boxs">
-    <div class="boxs-top clearfix">
-      <h3 class="top-title">智能硬件</h3>
-      <span class="top-sub">
-        查看全部
-        <i class="sub-icon fa fa-fw fa-chevron-circle-right"></i>
-      </span>
-    </div>
-    <div class="boxs-content">
-      <div class="box-left">
-        <img src="http://i3.mifile.cn/a4/57c2f4fb-6743-44c6-b447-6bd55aaeef89" alt="" />
+  <div class="boxs-container">
+    <div class="boxs">
+      <div class="boxs-top clearfix">
+        <h3 class="top-title">智能硬件</h3>
+        <span class="top-sub">
+          查看全部
+          <i class="sub-icon fa fa-fw fa-chevron-circle-right"></i>
+        </span>
       </div>
-      <div class="box-right">
-        <ul class="box-content">
-          <li class="box" v-for="good in goods">
-            <span></span>
-            <img :src="good.imgUrl" alt="" />
-            <h3 class="box-title">{{good.title}}</h3>
-            <p class="box-desc">
-              {{good.desc}}
-            </p>
-            <p class="price">
-              {{good.price}}
-            </p>
-          </li>
-        </ul>
+      <div class="boxs-content">
+        <div class="box-left">
+          <img src="http://i3.mifile.cn/a4/57c2f4fb-6743-44c6-b447-6bd55aaeef89" alt="" />
+        </div>
+        <div class="box-right">
+          <ul class="box-content">
+            <li class="box" v-for="good in goods">
+              <span></span>
+              <img :src="good.imgUrl" alt="" />
+              <h3 class="box-title">{{good.title}}</h3>
+              <p class="box-desc">
+                {{good.desc}}
+              </p>
+              <p class="price">
+                {{good.price}}
+              </p>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-	</div>
+  	</div>
+  </div>
 </template>
 
 <script>
